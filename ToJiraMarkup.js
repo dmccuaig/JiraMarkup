ccdbc4549f884ed5c1c09016f9beff11fd69504b
Dan <dan@dantrace.org>
2018-02-24 21:11:11 -0800

    Added table preview, jquery, and user persistence.

--- ToJiraMarkup.js
+++ ToJiraMarkup.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     var cbHasHeaders;
     var tbInput;
     var taOutput;
+    var cbShowPreview;
+    var divPreview;
 
     function getDelimiter() {
         var delimiter = document.querySelector('input[name="_delimitType"]:checked').value;
@@ -18,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function getHasHeaders() { return cbHasHeaders.checked; }
+    function getShowPreview() { return cbShowPreview.checked; }
 
     function parseText(input, delimiter) {
         var config = {
@@ -32,29 +35,24 @@ document.addEventListener("DOMContentLoaded", function () {
         if (table.length === 0) return "";
 
         var firstRow;
-        var rowStart;
         var i, j;
 
-        if (hasHeaders) {
-            firstRow = table[0];
-            rowStart = 1;
-        }
-        else {
+        if (!hasHeaders) {
             firstRow = Array(table[0].length);
             for (i = 0; i < firstRow.length; i++) {
                 firstRow[i] = "Column" + i;
             }
-            rowStart = 0;
+            table.unshift(firstRow);
         }
 
         var output = "";
-
+        firstRow = table[0];
         for (i = 0; i < firstRow.length; i++) {
             output += "||" + firstRow[i];
         }
         output += "||\n";
 
-        for (i = rowStart; i < table.length; i++) {
+        for (i = 1; i < table.length; i++) {
             for (j = 0; j < table[i].length; j++) {
                 output += "|" + table[i][j];
             }
@@ -62,6 +60,51 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         return output;
+    }
+
+
+    function setPreview(data, hasHeaders) {
+
+        options = {
+            th: hasHeaders,
+            thead: hasHeaders,
+            attrs: { border : "1"}
+    };
+
+        divPreview.innerHTML = null;
+        var htmlTable = arrayToTable(data, options);
+        $("#_preview").append(htmlTable);
+    }
+
+    function clearPreview() {
+        divPreview.innerHTML = null;
+    }
+
+    function saveChoices() {
+        var choices = {
+            version: 1,
+            hasHeaders: $("#_hasHeaders").is(":checked"),
+            delimiter: getDelimiter(),
+            preview: $("#_showPreview").is(":checked"),
+        };
+        chrome.storage.sync.set(choices);
+    }
+
+    function restoreChoices(choices) {
+        if(choices.hasHeaders)
+            $("#_hasHeaders").prop("checked", choices.hasHeaders);
+        if(choices.preview)
+            $("#_showPreview").prop("checked", choices.preview);
+        if (choices.delimiter) {
+            if (choices.delimiter === ",")
+                $("#_comma").prop("checked", true);
+            else if (choices.delimiter === "\t")
+                $("#_tab").prop("checked", true);
+            else {
+                $("#_other").prop("checked", true);
+                $("#_delimiter").val(choices.delimiter);
+            }
+        }
     }
 
     function makeTable() {
@@ -77,9 +120,18 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
+        saveChoices();
+
         var table = parseText(input, delimiter);
-        var output = makeMarkup(table, getHasHeaders());
+        var hasHeaders = getHasHeaders();
+        var output = makeMarkup(table, hasHeaders);
+
         setOutput(output);
+
+        if (getShowPreview())
+            setPreview(table, hasHeaders);
+        else
+            clearPreview();
     }
 
     function onCopyClicked() {
@@ -95,12 +147,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     taInput = setupInput('_input', 'input');
     cbHasHeaders = setupInput('_hasHeaders', 'change');
+    cbShowPreview = setupInput('_showPreview', 'change');
     setupInput('_tab', 'change');
     setupInput('_comma', 'change');
     setupInput('_other', 'change');
     tbInput = setupInput('_delimiter', 'input');
     taOutput = document.getElementById('_output');
+    divPreview = document.getElementById("_preview");
 
     document.getElementById('_copy').addEventListener('click', onCopyClicked);
 
+    chrome.storage.sync.get(null, restoreChoices);
 });
